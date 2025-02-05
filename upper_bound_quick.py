e037a22914816f54c797ddeaea50742bce05a5a0
Benjamin Leblanc <beni-leblanc@hotmail.com>
2025-02-05 14:31:58 -0500

    [PATCH] Now does not include b in initial x_weights.

--- upper_bound_quick.py
+++ upper_bound_quick.py
@@ -215,7 +215,7 @@ l = 0
 delta = 0.05
 a = 0#optimal_test_bound(1000, delta)
 b = 1
-x_weights = [0.2, 0.6, 0.8, 1]
+x_weights = [0.2, 0.6, 0.8]
 tol = 1e-5
 
 bound_optimization(n, l, a, b, x_weights, delta, tol)
